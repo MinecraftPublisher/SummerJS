7933d5482238dca3aeaf9a88f80a8a48d68c05ae
MinecraftPublisher <moroud86@gmail.com>
2021-07-05 10:38:55 +0430

    Update SummerJS.js

--- src/SummerJS.js
+++ src/SummerJS.js
@@ -6,12 +6,35 @@
  * 
  */
 
-let SummerJS = (query) => {
+let SummerJS = (query, data) => {
     if (document.querySelector("query") == null) {
         return { "error": "Invalid query selector." };
+    } else if (data == null || data == {}) {
+        return { "error": "Please give me some data!" };
     } else {
         /* Initialize the SummerJS object and return it. */
         let obj = {};
 
+        obj.error = "none";
+        obj.objects = [];
+        obj.solid = () => { return "solid"; };
+        obj.object = (name, sprite, data) => {
+            let made = {};
+            made.name = name;
+            made.sprite = btoa(sprite);
+            made.data = data;
+            made.type = "summer-obj";
+
+            return made;
+        };
+        obj.add = (data) => {
+            if (data.type == "summer-obj") {
+                obj.objects.push(data);
+
+                return { "error": "none" };
+            } else {
+                return { "error": "Not a SummerJS object." };
+            }
+        };
     }
 };
