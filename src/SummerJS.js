4f94221e23f1014de4bbec7624f35079f3837863
MinecraftPublisher <moroud86@gmail.com>
2021-07-05 10:13:14 +0430

    Made the document index and started working on SummerJS.

---
+++ src/SummerJS.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * SummerJS - By mforoud86
+ * Any and all donations/pull requests are appreciated.
+ * SummerJS is lisenced under the MIT License.
+ * 
+ */
+
+let SummerJS = (query) => {
+    if (document.querySelector("query") == null) {
+        return { "error": "Invalid query selector." };
+    } else {
+        /* Initialize the SummerJS object and return it. */
+        let obj = {};
+
+    }
+};
