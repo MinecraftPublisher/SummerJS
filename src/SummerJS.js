f0e7c8e8feeee06db5bb0b419ff62c33e16784ff
MinecraftPublisher <moroud86@gmail.com>
2021-07-06 10:21:07 +0430

    Pushed all commits to main.

--- src/SummerJS.js
+++ src/SummerJS.js
@@ -7,34 +7,69 @@
  */
 
 let SummerJS = (query, data) => {
-    if (document.querySelector("query") == null) {
-        return { "error": "Invalid query selector." };
-    } else if (data == null || data == {}) {
-        return { "error": "Please give me some data!" };
-    } else {
-        /* Initialize the SummerJS object and return it. */
-        let obj = {};
 
-        obj.error = "none";
-        obj.objects = [];
-        obj.solid = () => { return "solid"; };
-        obj.object = (name, sprite, data) => {
-            let made = {};
-            made.name = name;
-            made.sprite = btoa(sprite);
-            made.data = data;
-            made.type = "summer-obj";
+    SummerJS.inject = (code, isHTML) => {
+        /* If you are reading this, it either means that you are here to steal my source code, or to find out why your project is not loading on someone's computer. The answer is simple: Injection.
+        SummerJS uses javascript code injections in order to function correctly, but don't worry. I'll override this and mark it as an issue later myself if it made a lot of problems. */
 
-            return made;
-        };
-        obj.add = (data) => {
-            if (data.type == "summer-obj") {
-                obj.objects.push(data);
+        if (isHTML) {
+            let child = document.createElement("summerjs-injection");
+            child.setAttribute("notice", "This might get depreciated in later versions. Please, always update to the latest version.");
+            child.innerHTML = code;
+            child.setAttribute("randomness", Math.floor(Math.random() * 10000));
 
-                return { "error": "none" };
-            } else {
-                return { "error": "Not a SummerJS object." };
-            }
-        };
+            document.body.innerHTML += child.outerHTML;
+        } else {
+            let child = document.createElement("script");
+            child.setAttribute("notice", "This might get depreciated in later versions. Please, always update to the latest version.");
+            child.setAttribute("summerjs-injection", "");
+            child.setAttribute("randomness", Math.floor(Math.random() * 10000));
+            child.innerText = code;
+
+            document.body.innerHTML += child.outerHTML;
+        }
+    };
+
+    SummerJS.catchError = (error) => {
+        if (SummerJS.init) {
+            SummerJS.document.innerHTML += `<h3 summerjs-error style="color: red; font-family: 'Monolisa';">${error}</h3>`;
+        } else {
+            console.log(error);
+        }
+    };
+
+    if (SummerJS.init) {
+        return "SummerJS is already initialized.";
     }
+
+    if (document.querySelector(query) == null) {
+        SummerJS.catchError("Invalid query selector.");
+    } else {
+        SummerJS.init = true;
+        SummerJS.document = document.querySelector(query);
+
+        SummerJS.init = (width, height) => {
+            let canvas = document.createElement("summerjs");
+            canvas.style = `width: ${width}; height: ${height};`;
+
+
+            SummerJS.document.appendChild(canvas);
+            SummerJS.document = canvas;
+            SummerJS.init = () => {
+                return "Already initialized.";
+            };
+        };
+
+        if (data == {} || data == null || data == undefined) {
+            // Just run the init code.
+
+            SummerJS.init();
+        } else {
+            // Client has some data to work with.
+
+            SummerJS.init();
+        }
+    }
+
+    return SummerJS;
 };
